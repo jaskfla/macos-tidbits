8b3ded03e9336c7f4ee8ebda55aa9f00b537b7c0
Jasper Lai <33956381+jaskfla@users.noreply.github.com>
2025-03-25 22:40:52 +1300

    heading-anchors

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+import { IdAttributePlugin } from '@11ty/eleventy';
 import syntaxHighlight from '@11ty/eleventy-plugin-syntaxhighlight';
 import footnotes from 'eleventy-plugin-footnotes';
 import { minify } from 'html-minifier-terser';
@@ -8,10 +9,17 @@ export default (eleventyConfig) => {
 	eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/images`);
 	eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/style`);
 	eleventyConfig.addPassthroughCopy(`${SOURCE_DIR}/robots.txt`);
+	eleventyConfig.addPassthroughCopy({
+		'node_modules/@zachleat/heading-anchors/heading-anchors.js':
+			'scripts/heading-anchors.js',
+	});
 
 	eleventyConfig.addPlugin(footnotes, {
 		baseClass: 'footnotes',
 		classes: ['flow'],
+	});
+	eleventyConfig.addPlugin(IdAttributePlugin, {
+		selector: 'h2,h3,h4,h5,h6',
 	});
 	eleventyConfig.addPlugin(syntaxHighlight);
 
